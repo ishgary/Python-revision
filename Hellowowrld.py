5c74d0a5183b85d16221ad5ec57609b96b177b69
Ishantt Garg <34388507+ishgary@users.noreply.github.com>
2026-02-05 11:15:07 +0530

    First Post

---
+++ Hellowowrld.py
@@ -0,0 +1,22 @@
+print("Hellow world")
+print ( 2 ** 4)
+tuna = 10
+sushi = 12
+print (tuna + sushi )
+print("I don't think he's 18")
+print(' I don\'t think this is it') #this is an example of apostrophie in counuation 
+firstName = "David "
+lastName = "Bombal"
+# print({firstname} + {lastname})
+# Option 1: f-string (recommended)
+print(f"{firstName} {lastName}")
+
+# Option 2: concatenation
+print(firstName + " " + lastName)
+
+# Option 3: multiple args
+print(firstName, lastName)
+print(firstName * 5)
+print(firstName[2]) # this is to check character index of the string 
+print(firstName[2:5])
+print(len('aksjdhkahd'))
