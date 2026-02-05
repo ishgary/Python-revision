1fec0fb1268825799997ac119a51f9d0738acb34
Ishantt Garg <ishanttgarg@gmail.com>
2026-02-06 00:19:39 +0530

    if else

---
+++ If elif else.py
@@ -0,0 +1,9 @@
+age = 28
+if age < 25:
+    print("Under age")
+elif age > 30:
+    print("over age")
+elif age == 27:
+    print("right age, you can move forward")
+else :
+    print("right age, you can move forward 2")
